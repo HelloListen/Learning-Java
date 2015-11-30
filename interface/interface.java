d24b1e5dc2123a8c724e79b6ba912ab1b550651b
Listen <listenzhangbin@163.com>
2015-11-30 23:03:14 +0800

    innerClass & anonymousInnerClass

--- interface/interface.java
+++ interface/interface.java
@@ -19,30 +19,63 @@ public interface Powered extends Moveable {
     double SPEED_LIMIT = 95;
 }
 
-class Employee implements Cloneable,Comparable{
+class Employee implements Cloneable, Comparable {
     //...
 }
 
-class Employee implements Cloneable{
-    public Employee clone() throws CloneNotSupportedException{
+class Employee implements Cloneable {
+    public Employee clone() throws CloneNotSupportedException {
         return (Employee) super.clone();
     }
 }
 
-class Employee implements Cloneable{
-    public Employee clone() throws CloneNotSupportedException{
-        Employee cloned=(Employee) super.clone();
+class Employee implements Cloneable {
+    public Employee clone() throws CloneNotSupportedException {
+        Employee cloned = (Employee) super.clone();
 
-        cloned.hireDay=(Date) hireDay.clone();
+        cloned.hireDay = (Date) hireDay.clone();
         return cloned;
     }
 }
 
-class TimePrinter implements ActionListener{
-    public void actionPerformed(ActionEvent event){
-        Date now=new Date();
-        System.out.println("At the tone, the time is "+now);
+class TimePrinter implements ActionListener {
+    public void actionPerformed(ActionEvent event) {
+        Date now = new Date();
+        System.out.println("At the tone, the time is " + now);
         Toolkit.getDefaultToolkit().beep();
+    }
+}
+
+public class TalkingClock {
+    private int interval;
+    private boolean beep;
+
+    public TalkingClock(int interval, boolean beep) {...}
+
+    public void start() {...}
+
+    public class TimePrinter implements ActionListener {
+        public void actionPerformed(ActionEvent event) {
+            Date now = new Date();
+            System.out.println("At the tone, the time is " + now);
+            if (beep){
+                Toolkit.getDefaultToolkit().beep();
+            }
+        }
+    }
+
+    public void start(int interval,final boolean beep){
+        ActionListener listener=new ActionListener(){
+            public void actionPerformed(ActionEvent event){
+                Date now=new Date();
+                System.out.println("At the tone, the time is "+now);
+                if (beep){
+                    Toolkit.getDefaultToolkit().beep();
+                }
+            }
+        }
+        Timer t=new Timer(interval,listener);
+        t.start();
     }
 }
 
