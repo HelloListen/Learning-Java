3214a5c5a25c21d72f9b2fc4adf64606c8477c72
Listen <listenzhangbin@163.com>
2015-12-01 23:18:29 +0800

    jar

---
+++ interface/proxy/ProxyTest.java
@@ -0,0 +1,62 @@
+//package proxy;
+
+import java.lang.reflect.*;
+import java.util.*;
+
+/**
+ * This program demonstrates the use of proxies.
+ *
+ * @version 1.00 2000-04-13
+ */
+
+public class ProxyTest {
+    public static void main(String[] args) {
+        Object[] elements = new Object[1000];
+
+        for (int i = 0; i < elements.length; i++) {
+            Integer value = i + 1;
+            InvocationHandler handler = new TraceHandler(value);
+            Object proxy = Proxy.newProxyInstance(null, new Class[]{Comparable.class}, handler);
+            elements[i] = proxy;
+        }
+
+        Integer key = new Random().nextInt(elements.length) + 1;
+
+        int result = Arrays.binarySearch(elements, key);
+
+        if (result >= 0) {
+            System.out.println(elements[result]);
+        }
+    }
+}
+
+/**
+ * An invocation handler that prints out the method name and parameters, the
+ * invokes the original method
+ */
+class TraceHandler implements InvocationHandler {
+    private Object target;
+
+    /**
+     * Constructors a TraceHandler
+     * @param t the implicit paramter of the method call
+     */
+    public TraceHandler(Object t) {
+        target = t;
+    }
+
+    public Object invoke(Object proxy, Method m, Object[] args) throws Throwable {
+        System.out.print(target);
+        System.out.print("." + m.getName() + "(");
+        if (args != null) {
+            for (int i = 0; i < args.length; i++) {
+                System.out.print(args[i]);
+                if (i < args.length - 1) {
+                    System.out.print(",");
+                }
+            }
+        }
+        System.out.println(")");
+        return m.invoke(target, args);
+    }
+}
