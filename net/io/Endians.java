845dc902b655eaddac1d9334810865ccf333bcf4
Listen <listenzhangbin@163.com>
2016-01-17 23:19:57 +0800

    io

---
+++ net/io/Endians.java
@@ -0,0 +1,24 @@
+package io;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.util.Arrays;
+
+/**
+ * Created by zhangbin on 16/1/17.
+ */
+public class Endians {
+    public static void main(String[] args) {
+        ByteBuffer bb = ByteBuffer.wrap(new byte[12]);
+        bb.asCharBuffer().put("abcdef");
+        System.out.print(Arrays.toString(bb.array()));
+        bb.rewind();
+        bb.order(ByteOrder.BIG_ENDIAN);
+        bb.asCharBuffer().put("abcdef");
+        System.out.print(Arrays.toString(bb.array()));
+        bb.rewind();
+        bb.order(ByteOrder.LITTLE_ENDIAN);
+        bb.asCharBuffer().put("abcdef");
+        System.out.print(Arrays.toString(bb.array()));
+    }
+}
