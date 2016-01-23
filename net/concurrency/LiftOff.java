5f8f8b0f2abbd8ec41a37a74dc75f727b5dcc4aa
Listen <listenzhangbin@163.com>
2016-01-23 23:05:37 +0800

    annotation&concurrency

---
+++ net/concurrency/LiftOff.java
@@ -0,0 +1,28 @@
+package myconcurrency;
+
+/**
+ * Created by zhangbin on 16/1/23.
+ */
+public class LiftOff implements Runnable {
+    protected int countDown = 10;
+    private static int taskCount = 0;
+    private final int id = taskCount++;
+
+    public LiftOff() {
+    }
+
+    public LiftOff(int countDown) {
+        this.countDown = countDown;
+    }
+
+    public String status() {
+        return "#" + id + "(" + (countDown > 0 ? countDown : "LiftOff") + "),";
+    }
+
+    public void run() {
+        while (countDown-- > 0) {
+            System.out.print(status());
+            Thread.yield();
+        }
+    }
+}
