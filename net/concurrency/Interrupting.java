c19600bda2a7558829196a13162dda2c857f69ac
Listen <listenzhangbin@163.com>
2016-01-25 23:56:55 +0800

    synchronized&interrupt

---
+++ net/concurrency/Interrupting.java
@@ -0,0 +1,85 @@
+package myconcurrency;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by zhangbin on 16/1/25.
+ */
+class SleepBlocked implements Runnable {
+    public void run() {
+        try {
+            TimeUnit.SECONDS.sleep(100);
+        } catch (InterruptedException e) {
+            System.out.println("InterruptedException");
+        }
+        System.out.println("Exiting SleepBlocked.run()");
+    }
+}
+
+class IOBlocked implements Runnable {
+    private InputStream in;
+
+    public IOBlocked(InputStream is) {
+        in = is;
+    }
+
+    public void run() {
+        try {
+            System.out.println("Waiting for read():");
+            in.read();
+        } catch (IOException e) {
+            if (Thread.currentThread().isInterrupted()) {
+                System.out.println("Interrupted form bolcked I/O");
+            } else {
+                throw new RuntimeException(e);
+            }
+        }
+        System.out.println("Exiting IOBlocked.run()");
+    }
+}
+
+class SynchronizedBlocked implements Runnable {
+    public synchronized void f() {
+        while (true) {
+            Thread.yield();
+        }
+    }
+
+    public SynchronizedBlocked() {
+        new Thread() {
+            public void run() {
+                f();
+            }
+        }.start();
+    }
+
+    public void run() {
+        System.out.println("Exiting SynchronizedBlocked.run()");
+    }
+}
+
+public class Interrupting {
+    private static ExecutorService exec = Executors.newCachedThreadPool();
+
+    static void test(Runnable r) throws InterruptedException {
+        Future<?> f = exec.submit(r);
+        TimeUnit.MILLISECONDS.sleep(100);
+        System.out.println("Interrrupting " + r.getClass().getName());
+        f.cancel(true);
+        System.out.println("Interrupt sent to " + r.getClass().getName());
+    }
+
+    public static void main(String[] args) throws Exception {
+        test(new SleepBlocked());
+        test(new IOBlocked(System.in));
+        test(new SynchronizedBlocked());
+        TimeUnit.SECONDS.sleep(3);
+        System.out.println("Aborting with system.exit(0)");
+        System.exit(0);
+    }
+}
