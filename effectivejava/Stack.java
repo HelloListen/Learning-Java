35d38b4166847e9c9350754465d55c15f2513b92
Listen <listenzhangbin@163.com>
2016-01-31 23:05:11 +0800

    access

---
+++ effectivejava/Stack.java
@@ -0,0 +1,95 @@
+package effectivejava;
+
+import java.util.Arrays;
+import java.util.EmptyStackException;
+import java.util.Map;
+
+public class Stack {
+    private Object[] elements;
+    private int size = 0;
+    private static final int DEFAULT_INITIAL_CAPACITY = 16;
+
+    public Stack() {
+        this.elements = new Object[DEFAULT_INITIAL_CAPACITY];
+    }
+
+    public void push(Object e) {
+        ensureCapacity();
+        elements[size++] = e;
+    }
+
+    public Object pop() {
+        if (size == 0) {
+            throw new EmptyStackException();
+        }
+        Object result = elements[--size];
+        elements[size] = null;
+        return result;
+    }
+
+    public void ensureCapacity() {
+        if (elements.length == size) {
+            elements = Arrays.copyOf(elements, 2 * size + 1);
+        }
+    }
+
+    @Override
+    public Stack clone() {
+        try {
+            Stack result = (Stack) super.clone();
+            result.elements = elements.clone();
+            return result;
+        } catch (CloneNotSupportedException e) {
+            throw new AssertionError();
+        }
+    }
+}
+
+class HashTable implements Cloneable {
+//    private Map.Entry[] buckets =
+}
+
+class Point {
+    private double x;
+    private double y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+}
+
+final class Time {
+    private static final int HOURS_PER_DAY = 24;
+    private static final int MINUTES_PER_HOUR = 60;
+
+    public final int hour;
+    public final int minute;
+
+    public Time(int hour, int minute) {
+        if (hour < 0 || hour >= HOURS_PER_DAY) {
+            throw new IllegalArgumentException("Hour: " + hour);
+        }
+        if (minute < 0 || minute >= MINUTES_PER_HOUR) {
+            throw new IllegalArgumentException("Min: " + minute);
+        }
+        this.hour = hour;
+        this.minute = minute;
+    }
+}
