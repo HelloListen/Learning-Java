08b1e75f0bac319658c770f4dd6c9e570629c25e
Listen <listenzhangbin@163.com>
2016-04-17 23:32:18 +0800

    mybatis&data binding

---
+++ src/spring/ContactInfo.java
@@ -0,0 +1,33 @@
+package spring;
+
+/**
+ * Created by zhangbin on 16/4/17.
+ */
+public class ContactInfo {
+    private String phone;
+    private String address;
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "ContactInfo{" +
+                "phone='" + phone + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+}
