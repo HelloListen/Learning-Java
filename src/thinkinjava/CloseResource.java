a7cb114c3f06e79b9c196a7bfc2feb76b0824e5a
Listen <listenzhangbin@163.com>
2016-06-12 23:26:47 +0800

    spring

---
+++ src/thinkinjava/CloseResource.java
@@ -0,0 +1,30 @@
+package thinkinjava;
+
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by zhangbin on 16/6/5.
+ */
+public class CloseResource {
+    public static void main(String[] args) throws Exception {
+        ExecutorService exec = Executors.newCachedThreadPool();
+        ServerSocket server = new ServerSocket(8080);
+        InputStream socketInput = new Socket("localhost", 8080).getInputStream();
+        exec.execute(new IOBlocked(socketInput));
+        exec.execute(new IOBlocked(System.in));
+        TimeUnit.MILLISECONDS.sleep(100);
+        System.out.println("Shutting down all threads");
+        exec.shutdownNow();
+        TimeUnit.SECONDS.sleep(1);
+        System.out.println("Closing " + socketInput.getClass().getName());
+        socketInput.close();
+        TimeUnit.SECONDS.sleep(1);
+        System.out.println("Closing " + System.in.getClass().getName());
+        System.in.close();
+    }
+}
